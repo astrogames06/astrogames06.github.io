f4a05bd0e1178d7eda7b281bfe4bf23f0bc3019f
Bandit2710 <87123748+Bandit2710@users.noreply.github.com>
2023-11-17 21:11:31 +1100

    Update custom-elms.js

--- custom-elms.js
+++ custom-elms.js
@@ -93,7 +93,7 @@ class MyCustomElementTerms extends HTMLElement {
         <div class="footer-content">
           <p>
             &copy; 2023 Astro-Games, <br />
-            Made with <img class="fa-solid fa-heart" style="color: red;"></img> by our
+            Made with <i class="fa-solid fa-heart" id="red-filler" style="color: red;"></i> by our
             amazing community! <br /><br />
             <a href="../../legal/privacy.html">Privacy Policy <i class="fa-solid fa-arrow-right-from-bracket"></i></i></a>
             <br>
