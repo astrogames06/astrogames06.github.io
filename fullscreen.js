e1180a88cb2d814325b6006e918c975596fb1c3a
Astro <88717385+astrogames06@users.noreply.github.com>
2024-04-01 20:39:19 +1100

    Delete fullscreen.js

--- fullscreen.js
+++
@@ -1,8 +0,0 @@
-function goFullscreen(id) {
-    var element = document.getElementById(id);
-    if (element.mozRequestFullScreen) {
-      element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullScreen) {
-      element.webkitRequestFullScreen();
-    }
-}
