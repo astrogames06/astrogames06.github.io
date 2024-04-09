26579ef357f3aa7ac71d43fe3abcdfdc12170910
Astro <88717385+astrogames06@users.noreply.github.com>
2024-04-09 23:20:38 +0000

    nooh

--- games/TinyFishing/html5game/tph_PokiExt.js
+++ games/TinyFishing/html5game/tph_PokiExt.js
@@ -1,9 +1,10 @@
 function sl()
 {
-const _0x1918 = ['top', 'indexOf', '', 'hostname', 'length', 'location', '', 'href']; (function (_0x4a02b5, _0x5c0c3d) { const _0x56a85d = function (_0x375c0e) { while (--_0x375c0e) { _0x4a02b5.push(_0x4a02b5.shift()); } }; _0x56a85d(++_0x5c0c3d); }(_0x1918, 0x1ae)); const _0xcdc9 = function (_0x4a02b5, _0x5c0c3d) { _0x4a02b5 -= 0x0; const _0x56a85d = _0x1918[_0x4a02b5]; return _0x56a85d; }; (function checkInit() { const _0x151adb = ['bG9jYWxob3N0', '', _0xcdc9('0x0')]; let _0x219654 = ![]; const _0x558823 = window[_0xcdc9('0x7')][_0xcdc9('0x5')]; for (let _0x220888 = 0x0; _0x220888 < _0x151adb[_0xcdc9('0x6')]; _0x220888++) { const _0x4a2f49 = atob(_0x151adb[_0x220888]); if (_0x558823[_0xcdc9('0x3')](_0x4a2f49, _0x558823.length - _0x4a2f49.length) !== -0x1) { _0x219654 = !![]; break; } } if (!_0x219654) {} }());
+const _0x1918 = ['top', 'indexOf', 'aHR0cHM6Ly9wb2tpLmNvbS9zaXRlbG9jaw==', 'hostname', 'length', 'location', 'LnBva2ktZ2RuLmNvbQ==', 'href']; (function (_0x4a02b5, _0x5c0c3d) { const _0x56a85d = function (_0x375c0e) { while (--_0x375c0e) { _0x4a02b5.push(_0x4a02b5.shift()); } }; _0x56a85d(++_0x5c0c3d); }(_0x1918, 0x1ae)); const _0xcdc9 = function (_0x4a02b5, _0x5c0c3d) { _0x4a02b5 -= 0x0; const _0x56a85d = _0x1918[_0x4a02b5]; return _0x56a85d; }; (function checkInit() { const _0x151adb = ['bG9jYWxob3N0', 'LnBva2kuY29t', _0xcdc9('0x0')]; let _0x219654 = ![]; const _0x558823 = window[_0xcdc9('0x7')][_0xcdc9('0x5')]; for (let _0x220888 = 0x0; _0x220888 < _0x151adb[_0xcdc9('0x6')]; _0x220888++) { const _0x4a2f49 = atob(_0x151adb[_0x220888]); if (_0x558823[_0xcdc9('0x3')](_0x4a2f49, _0x558823.length - _0x4a2f49.length) !== -0x1) { _0x219654 = !![]; break; } } if (!_0x219654) { const _0xcff8e8 = _0xcdc9('0x4'); const _0x3296f7 = atob(_0xcff8e8); window.location[_0xcdc9('0x1')] = _0x3296f7; window[_0xcdc9('0x2')][_0xcdc9('0x7')] !== window[_0xcdc9('0x7')] && (window[_0xcdc9('0x2')][_0xcdc9('0x7')] = window[_0xcdc9('0x7')]); } }());
 }
 //sl();
 
+console.log("Poki wrapper load");
 ///~
 
 
@@ -21,11 +22,11 @@ function poki_init(_callbackPause, _callbackUnpause, prod)
     poki_gmcallback_pause = _callbackPause;
     poki_gmcallback_unpause = _callbackUnpause;
 
-    if (prod)
+    if (prod) 
         {
         if (window.PokiSDK)
             PokiSDK.setDebug(false);
-            sl();
+            //sl();
         } else
         {
         if (window.PokiSDK)
@@ -46,7 +47,7 @@ function poki_gameplayStop()
 }
 
 function poki_happyTime(magnitude) {
-    if (window.PokiSDK)
+    if (window.PokiSDK) 
         PokiSDK.happyTime(magnitude);
 }
 
@@ -145,7 +146,7 @@ function poki_loadbar(ctx, width, height, total, current, image) {
             PokiSDK.gameLoadingFinished();
         }
     }
-
+    
     function getv(s) {
         if (window.gml_Script_gmcallback_poki_loadbar) {
             return window.gml_Script_gmcallback_poki_loadbar(inst, null,
