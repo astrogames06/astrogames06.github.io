772cd71052d066add01462bcd998529c9088297e
Jesse Sher <jebba272727@gmail.com>
2025-09-21 00:07:56 +1000

    added description loading

--- scripts/load-game.js
+++ scripts/load-game.js
@@ -1,3 +1,5 @@
+// const { response } = require("express");
+
 function attachEventListeners() {
     let games_ul = document.getElementById('games-ul');
     if (!games_ul) {
@@ -29,7 +31,8 @@ if (window.location.pathname.endsWith('game.html')) {
     document.title = localStorage.getItem('game-title');
 
     let game_title = document.getElementById('game-title');
-    game_title.innerText = localStorage.getItem('game-title');
+    let game_title_item = localStorage.getItem('game-title');
+    game_title.innerText = game_title_item;
     let game_info = document.getElementById('game-info');
     game_info.innerText = localStorage.getItem('game-title');
 
@@ -40,6 +43,20 @@ if (window.location.pathname.endsWith('game.html')) {
     let game_name = game_title.innerText.split(' ').join('');
     let embededGame = document.getElementById('embededGame');
     let canvas = document.getElementsByClassName("#canvas")[0];
+
+    fetch('descrip.json')
+        .then(response => response.json())
+        .then(data => {
+            let game = data.find(item => item.game_name === game_title_item);
+            if (game) {
+                console.log(game.description);
+            } else {
+                console.log("GAME NOT FOUND.")
+                console.log(game_name);
+                console.log("Game not found.");
+            }
+        })
+        .catch(error => console.error("Error loading JSON:", error));
 
     fetch('games.json')
         .then(response => response.json())
